3351261680ad187363db0348bb6a6009c58e76d6
dujm <jdu523@outlook.com>
2019-09-07 22:45:29 +0200

    add theme

--- fishingbird/pelicanconf.py
+++ fishingbird/pelicanconf.py
@@ -33,3 +33,6 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# I added a theme
+THEME = '../pelican-themes/pelican-blue-penguin'
